1cf05529bfae8777b62a81043fc83046a13d027e
Kilian Lamberdière <kilian.lamberdiere@gmail.com>
2025-03-23 09:11:03 +0100

    FIX : 2 main issues. First one was NOT requesting the [game|cover|genre] ids to the API, resulting in data without any links. Second one was strongly coupling the view to the request, displaying them BEFORE every data are retrieved. (third one could be to complicate things with deferred)

--- app/src/main/java/com/insa/mygamelist/data/ApiService.kt
+++ app/src/main/java/com/insa/mygamelist/data/ApiService.kt
@@ -1,9 +1,7 @@
 package com.insa.mygamelist.data
 
 
-import android.provider.MediaStore.Audio.Genres
 import okhttp3.RequestBody
-import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiService {
